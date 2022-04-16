925a6fcdfee0dd1bf4eac20f4dba57f1f62f2ed1
Sultan <ss.softwareit@gmail.com>
2022-04-16 18:26:08 +0600

    'xml_ui'

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun numberAction(view: View) {}
+    fun operationAction(view: View) {}
+
+    fun allClearAction(view: View) {
+
+    }
+
+    fun backSpaceAction(view: View) {}
+    fun equalsAction(view: View) {}
+
 }
