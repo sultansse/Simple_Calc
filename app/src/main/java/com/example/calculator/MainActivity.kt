9b10efc328577054c1183c05c57f6eca5aa310e6
Sultan <ss.softwareit@gmail.com>
2022-04-17 16:46:57 +0600

    'working_operators'

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -49,11 +49,122 @@ class MainActivity : AppCompatActivity() {
         resultsTV.text = ""
     }
 
-    fun backSpaceAction(view: View) {
+    fun backSpaceAction(view: View)
+    {
         val length = workingsTV.length()
         if (length > 0 )
             workingsTV.text = workingsTV.text.subSequence(0, length - 1)
     }
-    fun equalsAction(view: View) {}
+
+    fun equalsAction(view: View)
+    {
+        resultsTV.text = calculateResult()
+    }
+
+    private fun calculateResult():String
+    {
+        val digitsOperators = digitsOperators()
+        if(digitsOperators.isEmpty()) return ""
+
+        val timesDivision = timesDivisionCalculate(digitsOperators)
+        if(timesDivision.isEmpty()) return ""
+
+        val result = addSubtractCalculate(timesDivision)
+        return result.toString()
+    }
+
+    private fun addSubtractCalculate(passedList: MutableList<Any>): Float
+    {
+        var result = passedList[0] as Float
+
+        for(i in passedList.indices)
+        {
+            if(passedList[i] is Char && i != passedList.lastIndex)
+            {
+                val operator = passedList[i]
+                val nextDigit = passedList[i + 1] as Float
+                if (operator == '+')
+                    result += nextDigit
+                if (operator == '-')
+                    result -= nextDigit
+            }
+        }
+
+        return result
+    }
+
+    private fun timesDivisionCalculate(passedList: MutableList<Any>): MutableList<Any>
+    {
+        var list = passedList
+        while (list.contains('x') || list.contains('/'))
+        {
+            list = calcTimesDiv(list)
+        }
+        return list
+    }
+
+    private fun calcTimesDiv(passedList: MutableList<Any>): MutableList<Any>
+    {
+        val newList = mutableListOf<Any>()
+        var restartIndex = passedList.size
+
+        for(i in passedList.indices)
+        {
+            if(passedList[i] is Char && i != passedList.lastIndex && i < restartIndex)
+            {
+                val operator = passedList[i]
+                val prevDigit = passedList[i - 1] as Float
+                val nextDigit = passedList[i + 1] as Float
+                when(operator)
+                {
+                    'x' ->
+                    {
+                        newList.add(prevDigit * nextDigit)
+                        restartIndex = i + 1
+                    }
+                    '/' ->
+                    {
+                        newList.add(prevDigit / nextDigit)
+                        restartIndex = i + 1
+                    }
+                    else ->
+                    {
+                        newList.add(prevDigit)
+                        newList.add(operator)
+                    }
+                }
+            }
+
+            if(i > restartIndex)
+                newList.add(passedList[i])
+        }
+        return newList
+    }
+
+    private fun digitsOperators():MutableList<Any>
+    {
+        val list = mutableListOf<Any>()
+        var currentDigit = "";
+        for (character in workingsTV.text)
+        {
+            if ( character.isDigit() || character == '.')
+                currentDigit += character
+            else
+            {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(character)
+            }
+        }
+
+        if (currentDigit != "")
+            list.add(currentDigit.toFloat())
+
+        return list
+    }
+
 
 }
+
+
+
